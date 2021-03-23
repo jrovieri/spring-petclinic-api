fce40b12c3f8001610f80b781c28fac699deef2c
Jonathan Rovieri <jrovieri@gmail.com>
2021-03-23 18:29:13 -0300

    spring init project

---
+++ src/main/java/io/jrovieri/petclinic/api/SpringPetClinicApiApplication.java
@@ -0,0 +1,13 @@
+package io.jrovieri.petclinic.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetClinicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jrovieri/petclinic/api/SpringPetClinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jrovieri.petclinic.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
